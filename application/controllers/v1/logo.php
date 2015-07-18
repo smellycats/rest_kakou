7ad7ec0f627f48c9e9c788e24204b9933fb7c902
SmellyCat <smellycat2014@foxmail.com>
2015-07-18 20:49:55 +0800

    Update logo remove useless code

--- application/controllers/v1/logo.php
+++ application/controllers/v1/logo.php
@@ -187,26 +187,23 @@ class Logo extends Parsing_Controller
             $carinfo_id = $query->num_rows == 0 ? 0 : $query->row()->id;
             $this->Mlogo->addFresh(array('user_id'=>$user_id, 'carinfo_id'=>$carinfo_id, 'modified'=>mdate('%Y-%m-%d %H:%m:%s')));
         } else {
-            #var_dump('test');
             $q_arr['id'] = $fresh->row()->carinfo_id;
             $i = 0;
             while ($i < 120){
-                #var_dump('test123');
                 $query = $this->Mlogo->getFresh($q_arr);
                 if ($query->num_rows() == 0){
-                    $i++;
-                    #continue;
+                    $i ++;
                 } else {
                     $carinfo_id = $query->row()->id;
                     $this->Mlogo->setFresh($user_id, array('carinfo_id'=>$carinfo_id, 'modified'=>mdate('%Y-%m-%d %H:%m:%s')));
                     break;
                 }
+                # 休眠250毫秒
                 usleep(250000);
-                #var_dump($i);
             }
         }
         $result['items'] = $query->result_array();
-        $result['total_num'] = $query->num_rows();
+        $result['total_count'] = $query->num_rows();
         foreach($result['items'] as $id=>$row) {
             $row['imgurl'] = 'http://' . $this->imgip[$row['img_ip']] . '/SpreadData' . $row['img_disk'] . '/' . $row['img_path'];
             unset($row['img_ip']);
