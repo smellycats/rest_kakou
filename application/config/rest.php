d2176de4240952dfad6c345afb0a7ae06c2f77ed
SmellyCat <smellycat2014@foxmail.com>
2015-10-27 11:56:06 +0800

    Update config rest

--- application/config/rest.php
+++ application/config/rest.php
@@ -173,7 +173,7 @@ $config['rest_valid_logins'] = array('admin' => '12345');
 |	 restrict certain methods to IPs in your whitelist
 |
 */
-$config['rest_ip_whitelist_enabled'] = true;
+$config['rest_ip_whitelist_enabled'] = false;
 
 /*
 |--------------------------------------------------------------------------
@@ -263,7 +263,7 @@ $config['rest_keys_table'] = 'keys';
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_keys'] = TRUE;
+$config['rest_enable_keys'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
