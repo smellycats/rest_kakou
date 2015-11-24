ae87676064fe8052786c0f29453cd9d889e32f2c
SmellyCat <smellycat2014@foxmail.com>
2015-11-24 17:01:37 +0800

    添加 models mtest

---
+++ application/models/mtest.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mtest extends CI_Model
+{
+	#private $logo_db;
+    /**
+     * Construct a mlogo instance
+     *
+     */
+	public function __construct()
+	{
+		parent::__construct();
+
+		#$this->logo_db = $this->load->database('logo_db', TRUE);
+	}
+
+}
+?>
+
